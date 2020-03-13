507ff5f9a89287b591be9c72d9e4238476404284
Benjamin Harrison <ben6@uw.edu>
2020-03-13 09:19:56 -0700

    Create pca_set3.R

---
+++ data/LC_MS_data/pca_set3.R
@@ -0,0 +1,37 @@
+pca_set3 <- function(input, sample_df, title){
+  pca <- prcomp(t(na.omit(input)),scale. = TRUE)
+  var_exp = pca$sdev^2
+  pve= round((var_exp/sum(var_exp))*100,1)
+  print(pve[1:3])
+  
+  pca <- as.data.frame(pca$x[,1:5])
+  pca <- merge(pca, sample_df, by.x ="row.names", by.y = "Sample.ID")
+  pca = pca[match(sample_df$Sample.ID, pca$Row.names),]
+  
+
+  pca.3d = plot_ly(data = pca, x= ~PC1, y= ~PC2, z= ~PC3, 
+                   color = ~sampleWeight, 
+                   size = ~runOrder,
+                   sizes= c(10,70),
+                   hoverinfo="text", 
+                   text = ~paste0("<br>Sample:",Row.names,
+                                  "<br>trait:",trait,
+                                  "<br>treatment:",treatment,
+                                  "<br>Run.order:",runOrder,
+                                  "<br>line.weight:",line.weight,
+                                  "<br>batch:",block),
+                   marker = list(sizemode = "diameter", opacity=0.6))%>%
+    add_markers() %>%
+    layout(title = "3D PCA - sampleWeight coded by color, size by runOrder",
+           legend = list(size =10),
+           scene = list(xaxis = list(title = "PC1"),
+                        yaxis = list(title = "PC2"),
+                        zaxis = list(title = "PC3")))
+  
+  setwd("plots")
+  htmlwidgets::saveWidget(pca.3d,paste0(title,"_PCA_3D.html"))
+  setwd("..")
+  
+  return(list("pca_df"=pca))
+  
+}
